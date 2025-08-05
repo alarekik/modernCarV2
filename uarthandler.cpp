7e9f1ebc21742cefacedfad44f65648eb1037846
alarekik <alarekikmahres@gmail.com>
2025-08-05 17:13:13 +0100

    auto dection radar

--- uarthandler.cpp
+++ uarthandler.cpp
@@ -11,69 +11,56 @@ uarthandler::uarthandler(QObject *parent)
 
 bool uarthandler::openPort()
 {
-    // foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
-    //     if (info.hasProductIdentifier() && info.productIdentifier() == 60016) {
-    //         m_serialport->setPort(info);
-    //         break;
-    //     }
-    // }
-    //---------------------------------------------------------------------------
-
-        foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
-            qDebug() << "Port:" << info.portName()
-            << "| Description:" << info.description()
-            << "| Vendor ID:" << info.vendorIdentifier()
-            << "| Product ID:" << info.productIdentifier();
-
-            // Match by Product ID and Vendor ID
-            if (info.vendorIdentifier() == 4292 && info.productIdentifier() == 60016) {
-                m_serialport->setPort(info);
-                m_serialport->setBaudRate(QSerialPort::Baud9600);
-                m_serialport->setDataBits(QSerialPort::Data8);
-                m_serialport->setParity(QSerialPort::NoParity);
-                m_serialport->setStopBits(QSerialPort::OneStop);
-                m_serialport->setFlowControl(QSerialPort::NoFlowControl);
-
-                if (m_serialport->open(QIODevice::ReadOnly)) {
-                    qDebug() << "Connected to" << info.portName();
-                    return true;
-                } else {
-                    qDebug() << "Failed to open port" << info.portName();
-                    return false;
+    QByteArray datatest;
+    QString messagetest;
+    bool found = false;
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
+        qDebug() << "Port:" << info.portName()
+        << "| Description:" << info.description()
+        << "| Vendor ID:" << info.vendorIdentifier()
+        << "| Product ID:" << info.productIdentifier();
+        if (info.vendorIdentifier() == 4292 && info.productIdentifier() == 60016) {
+            QSerialPort *testPort = new QSerialPort(info);
+            testPort->setBaudRate(QSerialPort::Baud9600);
+            testPort->setDataBits(QSerialPort::Data8);
+            testPort->setParity(QSerialPort::NoParity);
+            testPort->setStopBits(QSerialPort::OneStop);
+            testPort->setFlowControl(QSerialPort::NoFlowControl);
+            if (testPort->open(QIODevice::ReadOnly)) {
+                qDebug() << "Testing port:" << info.portName();
+                if (testPort->waitForReadyRead(200)) {
+                    datatest = testPort->readAll();
+                    messagetest = QString::fromLatin1(datatest.toHex(' ').toUpper());
+                    qDebug() << "Received data:" << messagetest;
+                    if (!messagetest.isEmpty()) {
+                        qDebug() << "Communication port found:" << info.portName();
+                        testPort->close();
+                        delete testPort;
+                        m_serialport->setPortName(info.portName());
+                        m_serialport->setBaudRate(QSerialPort::Baud9600);
+                        m_serialport->setDataBits(QSerialPort::Data8);
+                        m_serialport->setParity(QSerialPort::NoParity);
+                        m_serialport->setStopBits(QSerialPort::OneStop);
+                        m_serialport->setFlowControl(QSerialPort::NoFlowControl);
+                        if (m_serialport->open(QIODevice::ReadOnly)) {
+                            found = true;
+                            break;  // ✅ Stop testing once a valid port is found
+                        } else {
+                            qDebug() << "Failed to open m_serialport.";
+                        }
+                    }
                 }
-                qDebug() << "Target device not found.";
+                testPort->close();
+            } else {
+                qDebug() << "Failed to open test port:" << info.portName();
             }
-
-
+            delete testPort;
         }
-        qDebug() << "Target device not found.";
+    }
+    if (!found) {
+        qDebug() << "No communication port found.";
         return false;
-
-
-
-
-
-    //-----------------------------------------------------------------------------
-    // foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
-    //     if (info.vendorIdentifier()==4292 && info.productIdentifier() == 60016){
-    //         m_serialport->setPort(info);
-    //         m_serialport->setBaudRate(QSerialPort::Baud9600);
-    //         m_serialport->setDataBits(QSerialPort::Data8);
-    //         m_serialport->setParity(QSerialPort::NoParity);
-    //         m_serialport->setStopBits(QSerialPort::OneStop);
-    //         m_serialport->setFlowControl(QSerialPort::NoFlowControl);
-    //         m_serialport->open(QIODevice::ReadOnly);
-    //     }
-
-    // }
-    //------------------------------------------------
-    // m_serialport->setPortName("/dev/ttyUSB2");
-    // m_serialport->setBaudRate(QSerialPort::Baud9600);
-    // m_serialport->setDataBits(QSerialPort::Data8);
-    // m_serialport->setParity(QSerialPort::NoParity);
-    // m_serialport->setStopBits(QSerialPort::OneStop);
-    // m_serialport->setFlowControl(QSerialPort::NoFlowControl);
-    // m_serialport->open(QIODevice::ReadOnly);
+    }
     return true;
 }
 
